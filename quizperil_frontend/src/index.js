ba7b841a380741df34af4c37ad92095850bd25d8
github username <github email address>
2020-05-03 15:38:19 -0700

    testing stuff

--- quizperil_frontend/src/index.js
+++ quizperil_frontend/src/index.js
@@ -1,1 +1,10 @@
 // Maybe we should make different js files and this folder and call them here?
+const BASEURL = "http://localhost:3000/questions/"
+
+function showQuestion() {
+    fetch(BASEURL)
+    .then(resp => resp.json())
+    .then(console.log)
+}
+
+showQuestion()
