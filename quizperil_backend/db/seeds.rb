aa770ec9b10a3b32677cf4b531e5c84c0cdc4a31
Roger Robertson Jr <rjrjr@pm.me>
2020-05-03 13:33:55 -0700

    generated resources for backend

--- quizperil_backend/db/seeds.rb
+++ quizperil_backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Max of 50 questions per API call to opentdb
+# Generate session token: https://opentdb.com/api_token.php?command=request
+# Use session token in calls to get unique questions: https://opentdb.com/api.php?amount=10&token=YOURTOKENHERE
+# Reset token: https://opentdb.com/api_token.php?command=reset&token=YOURTOKENHERE
+# Tokens are deleted after 6 hours.
+# JSON.parse(RestClient.get(url)) #=> ruby data structure 
