2eb08dc378c50fb872925504940ed893047aa062
github username <github email address>
2020-05-04 10:46:46 -0700

    got a question to show on the page

--- quizperil_frontend/src/index.js
+++ quizperil_frontend/src/index.js
@@ -1,10 +1,1 @@
 // Maybe we should make different js files and this folder and call them here?
-const BASEURL = "http://localhost:3000/questions/"
-
-function showQuestion() {
-    fetch(BASEURL)
-    .then(resp => resp.json())
-    .then(console.log)
-}
-
-showQuestion()
--- quizperil_frontend/src/showQuestion.js
+++ quizperil_frontend/src/showQuestion.js
@@ -1,7 +1,48 @@
 const BASEURL = "http://localhost:3000/questions/"
 
-function showQuestion() {
+const div = document.getElementById("quiz_question")
+const h4 = document.getElementById("question")
+const form = document.getElementById("answers")
+const submit = document.getElementById("submit")
+
+function fetchQuestions() {
     fetch(BASEURL)
     .then(resp => resp.json())
-    .then(console.log)
+    .then(questions => showQuestion(questions))
 }
+
+function showQuestion(questions) {
+    let question = questions[0]
+    console.log(question)
+    h4.innerHTML = question.question
+
+    let inp = document.createElement("input")
+    inp.type = "radio"
+    inp.id = "correct"
+    inp.name = "question"
+    inp.value = "correct"
+
+    let correct = document.createElement("label")
+    correct.innerHTML = question.correct_answer
+    correct.for = "correct"
+
+    form.appendChild(inp)
+    form.appendChild(correct)
+
+    question.incorrect_answers.forEach((answer, id) => {
+        let inp = document.createElement("input")
+        inp.type = "radio"
+        inp.id = id
+        inp.name = "question"
+        inp.value = id
+
+        let ans = document.createElement("label")
+        ans.innerHTML = answer
+        ans.for = id
+
+        form.appendChild(inp)
+        form.appendChild(ans)
+    })
+}
+
+fetchQuestions()
