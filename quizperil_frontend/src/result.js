1fcaf6d6af96530cfb6c7ed85cd1c9c449a20edc
Roger Robertson Jr <rjrjr@pm.me>
2020-05-05 14:54:54 -0700

    moved elements to html

--- quizperil_frontend/src/result.js
+++ quizperil_frontend/src/result.js
@@ -5,26 +5,15 @@ const QUIZURL = "http://localhost:3000/quizzes"
 let quizQuestion = document.getElementById('quiz_question')
 quizQuestion.classList.toggle('hidden')
 
-// Create result elements
-let resultsContainer = document.createElement('div')
-resultsContainer.id = "results"
+// update result elements
+let rightContainer = document.getElementById('number_right')
+rightContainer.innerText = `Correct: ${numRight}`
 
+let wrongContainer = document.getElementById('number_wrong')
+wrongContainer.innerText = `Incorrect: ${numWrong}`
 
-let rightContainer = document.createElement('p')
-rightContainer.id = "number_right"
-rightContainer.innerText = `Correct: ${num_right}`
-
-let wrongContainer = document.createElement('p')
-wrongContainer.id = "number_wrong"
-wrongContainer.innerText = `Incorrect: ${num_wrong}`
-
-let totalContainer = document.createElement('p')
-totalContainer.id = "total"
-totalContainer.innerText = `Total: ${total}`
-
-resultsContainer.append(rightContainer, wrongContainer, totalContainer)
-
-document.body.appendChild(resultsContainer)
+let totalContainer = document.getElementById('total')
+totalContainer.innerText = `Total: ${numRight + numWrong}`
 
 // wrap in method to fire when timer expires
 function quizPost() {
@@ -44,12 +33,6 @@ function quizPost() {
 
 
 // Percentage? Snarky comments?
-
-// Button to start new quiz
-let quizBtn = document.createElement('button')
-quizBtn.id = "quiz_button"
-quizBtn.innerText = "Start a new quiz!"
-document.body.appendChild(quizBtn)
 
 quizBtn.addEventListener('click', function() {
     // toggle results off
